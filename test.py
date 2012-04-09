d5192e107abf0eca5bd864dde704c084e3d1d5cf
clach04 <Chris.Clark@actian.com>
2012-04-09 13:53:34 -0700

    Jython 2.2 includes _some_ py 2.3 features. These changes add Cpython 2.2 support.

--- test.py
+++ test.py
@@ -17,13 +17,22 @@ except ImportError:
     # TODO import simplejson, etc.
     json = None
 
-from __init__ import Stache, render, render_js
+from stache import Stache, render, render_js
 
 
 skip_bool = False
 if sys.version_info < (2, 3):
     skip_bool = True
 
+def mydict(*args, **kwargs):
+    """Emulate Python 2.3 dict keyword support
+    """
+    if args:
+        raise NotImplementedError('args not supported')
+    if kwargs:
+        return kwargs
+    else:
+        return {}
 
 def verify(output, template, data):
     print("%s with %s" % (template, data))
@@ -105,65 +114,65 @@ def bare_partial(stachio, output, template, data={}):
 
 def test(method=bare):
     # test tag lookup
-    yield method, 'a10c', 'a{{b}}c', dict(b=10)
-    yield method, 'a10c', 'a{{b}}c', dict(b=10)
-    yield method, 'ac', 'a{{b}}c', dict(c=10)
-    yield method, 'a10c', 'a{{b}}c', dict(b='10')
-    yield method, 'acde', 'a{{!b}}cde', dict(b='10')
+    yield method, 'a10c', 'a{{b}}c', mydict(b=10)
+    yield method, 'a10c', 'a{{b}}c', mydict(b=10)
+    yield method, 'ac', 'a{{b}}c', mydict(c=10)
+    yield method, 'a10c', 'a{{b}}c', mydict(b='10')
+    yield method, 'acde', 'a{{!b}}cde', mydict(b='10')
     if not skip_bool:
-        yield method, 'aTrue', 'a{{b}}', dict(b=True)
-    yield method, 'a123', 'a{{b}}{{c}}{{d}}', dict(b=1,c=2,d=3)
+        yield method, 'aTrue', 'a{{b}}', mydict(b=True)
+    yield method, 'a123', 'a{{b}}{{c}}{{d}}', mydict(b=1,c=2,d=3)
     # test falsy #sections
     if not skip_bool:
-        yield method, 'ab', 'a{{#b}}b{{/b}}', dict(b=True)
-        yield method, 'a', 'a{{^b}}b{{/b}}', dict(b=True)
-        yield method, 'a', 'a{{#b}}b{{/}}', dict(b=False)
-        yield method, 'ab', 'a{{^b}}b{{/b}}', dict(b=False)
+        yield method, 'ab', 'a{{#b}}b{{/b}}', mydict(b=True)
+        yield method, 'a', 'a{{^b}}b{{/b}}', mydict(b=True)
+        yield method, 'a', 'a{{#b}}b{{/}}', mydict(b=False)
+        yield method, 'ab', 'a{{^b}}b{{/b}}', mydict(b=False)
     #test invert sections
-    yield method, 'ab', 'a{{#b}}ignore me{{/b}}{{^b}}b{{/}}', dict(b=[])
-    yield method, 'ab', 'a{{#b}}b{{/b}}{{^b}}ignore me{{/}}', dict(b=[1])
+    yield method, 'ab', 'a{{#b}}ignore me{{/b}}{{^b}}b{{/}}', mydict(b=[])
+    yield method, 'ab', 'a{{#b}}b{{/b}}{{^b}}ignore me{{/}}', mydict(b=[1])
     if not skip_bool:
-        yield method, 'ab', 'a{{#b}}b{{/b}}{{^b}}ignore me{{/}}', dict(b=True)
+        yield method, 'ab', 'a{{#b}}b{{/b}}{{^b}}ignore me{{/}}', mydict(b=True)
     #test ?sections
-    yield method, 'a- 1 2 3 4', 'a{{?b}}-{{#b}} {{.}}{{/}}{{/}}', dict(b=[1,2,3,4])
+    yield method, 'a- 1 2 3 4', 'a{{?b}}-{{#b}} {{.}}{{/}}{{/}}', mydict(b=[1,2,3,4])
     if not skip_bool:
-        yield method, 'a', 'a{{?b}}ignoreme{{/}}', dict(b=False)
-    yield method, 'a', 'a{{?b}}ignoreme{{/}}', dict(b=[])
-    yield method, 'a', 'a{{?b}}ignoreme{{/}}', dict()
-    yield method, 'abb', 'a{{?b}}b{{/}}{{?b}}b{{/}}', dict(b=[1,2,3])
-    yield method, 'ab123d', 'a{{?b}}b{{#b}}{{.}}{{/}}d{{/}}', dict(b=[1,2,3])
+        yield method, 'a', 'a{{?b}}ignoreme{{/}}', mydict(b=False)
+    yield method, 'a', 'a{{?b}}ignoreme{{/}}', mydict(b=[])
+    yield method, 'a', 'a{{?b}}ignoreme{{/}}', mydict()
+    yield method, 'abb', 'a{{?b}}b{{/}}{{?b}}b{{/}}', mydict(b=[1,2,3])
+    yield method, 'ab123d', 'a{{?b}}b{{#b}}{{.}}{{/}}d{{/}}', mydict(b=[1,2,3])
     #test #section scope
-    yield method, 'abbbb', 'a{{#b}}b{{/b}}', dict(b=[1,2,3,4])
-    yield method, 'a1234', 'a{{#b}}{{.}}{{/b}}', dict(b=[1,2,3,4])
-    yield method, 'a a=1 a=2 a=0 a', 'a {{#b}}a={{a}} {{/b}}a', dict(a=0,b=[{'a':1},{'a':2},{'c':1}])
-    yield method, '1', '{{#a}}{{#b}}{{c}}{{/b}}{{/a}}', dict(a={'b':{'c':1}})
-    yield method, '12', '{{#a}}{{#b}}{{c}}{{/}}{{/a}}', dict(a={'b':[{'c':1}, {'c':2}]})
-    yield method, '12', '{{#a}}{{#b}}{{c}}{{/b}}{{/}}', dict(a=[{'b':{'c':1}},{'b':{'c':2}}])
-    yield method, '132', '{{#a}}{{#b}}{{c}}{{/}}{{/}}', dict(a=[{'b':[{'c':1}, {'c':3}]},{'b':{'c':2}}])
-    yield method, '132456', '{{#a}}{{#b}}{{c}}{{/b}}{{/a}}', dict(a=[{'b':[{'c':1}, {'c':3}]},{'b':{'c':2}},{'b':[{'c':4}, {'c':5}]},{'b':{'c':6}}])
-    yield method, '1', '{{#a}}{{#a}}{{c}}{{/a}}{{/a}}', dict(a={'a':{'c':1}})
+    yield method, 'abbbb', 'a{{#b}}b{{/b}}', mydict(b=[1,2,3,4])
+    yield method, 'a1234', 'a{{#b}}{{.}}{{/b}}', mydict(b=[1,2,3,4])
+    yield method, 'a a=1 a=2 a=0 a', 'a {{#b}}a={{a}} {{/b}}a', mydict(a=0,b=[{'a':1},{'a':2},{'c':1}])
+    yield method, '1', '{{#a}}{{#b}}{{c}}{{/b}}{{/a}}', mydict(a={'b':{'c':1}})
+    yield method, '12', '{{#a}}{{#b}}{{c}}{{/}}{{/a}}', mydict(a={'b':[{'c':1}, {'c':2}]})
+    yield method, '12', '{{#a}}{{#b}}{{c}}{{/b}}{{/}}', mydict(a=[{'b':{'c':1}},{'b':{'c':2}}])
+    yield method, '132', '{{#a}}{{#b}}{{c}}{{/}}{{/}}', mydict(a=[{'b':[{'c':1}, {'c':3}]},{'b':{'c':2}}])
+    yield method, '132456', '{{#a}}{{#b}}{{c}}{{/b}}{{/a}}', mydict(a=[{'b':[{'c':1}, {'c':3}]},{'b':{'c':2}},{'b':[{'c':4}, {'c':5}]},{'b':{'c':6}}])
+    yield method, '1', '{{#a}}{{#a}}{{c}}{{/a}}{{/a}}', mydict(a={'a':{'c':1}})
     if not skip_bool:
         yield method, '<3><3><3>', '<{{id}}><{{# a? }}{{id}}{{/ a? }}><{{# b? }}{{id}}{{/ b? }}>', {'id':3,'a?':True, 'b?':True}
     #test delim
-    yield method, 'delim{{a}}', '{{=<% %>=}}<%a%>{{a}}', dict(a='delim')
-    yield method, 'delim{{a}}delim<%a%>', '{{=<% %>=}}<%a%>{{a}}<%={{ }}=%>{{a}}<%a%>', dict(a='delim')
+    yield method, 'delim{{a}}', '{{=<% %>=}}<%a%>{{a}}', mydict(a='delim')
+    yield method, 'delim{{a}}delim<%a%>', '{{=<% %>=}}<%a%>{{a}}<%={{ }}=%>{{a}}<%a%>', mydict(a='delim')
     #test :
-    yield method, '123test', '123{{:hi}}abc{{/}}', dict(hi='test')
-    yield method, '123test', '123{{:hi}}{{:hi}}abc{{/}}{{/}}', dict(hi='test')
-    yield method, '123abc', '123{{:hi}}{{:hi2}}abc{{/}}{{/}}', dict()
-    yield method, '123cba', '123{{:hi}}{{:hi2}}abc{{/}}{{/}}', dict(hi2='cba')
-    yield method, '123abc', '123{{:hi}}abc{{/}}', dict()
+    yield method, '123test', '123{{:hi}}abc{{/}}', mydict(hi='test')
+    yield method, '123test', '123{{:hi}}{{:hi}}abc{{/}}{{/}}', mydict(hi='test')
+    yield method, '123abc', '123{{:hi}}{{:hi2}}abc{{/}}{{/}}', mydict()
+    yield method, '123cba', '123{{:hi}}{{:hi2}}abc{{/}}{{/}}', mydict(hi2='cba')
+    yield method, '123abc', '123{{:hi}}abc{{/}}', mydict()
     if not skip_bool:
-        yield method, '123abc', '123{{:hi}}abc{{/}}', dict(hi=False)
-    yield method, '123abc', '123{{:hi}}abc{{/}}', dict(hi=[])
-    yield method, '123test', '{{<hi}}test{{/}}123{{:hi}}abc{{/}}', dict()
+        yield method, '123abc', '123{{:hi}}abc{{/}}', mydict(hi=False)
+    yield method, '123abc', '123{{:hi}}abc{{/}}', mydict(hi=[])
+    yield method, '123test', '{{<hi}}test{{/}}123{{:hi}}abc{{/}}', mydict()
     #iterators
-    yield method, '0123456789', '{{#a}}{{.}}{{/a}}', dict(a=range(10))
-    yield method, '02468', '{{#a}}{{.}}{{/a}}', dict(a=list(filter(lambda x: x%2==0, range(10))))
+    yield method, '0123456789', '{{#a}}{{.}}{{/a}}', mydict(a=range(10))
+    yield method, '02468', '{{#a}}{{.}}{{/a}}', mydict(a=list(filter(lambda x: x%2==0, range(10))))
     #escaping
-    yield method, '&gt;&lt;', '{{a}}', dict(a='><')
-    yield method, '><', '{{&a}}', dict(a='><')
-    yield method, '><', '{{{a}}}', dict(a='><')
+    yield method, '&gt;&lt;', '{{a}}', mydict(a='><')
+    yield method, '><', '{{&a}}', mydict(a='><')
+    yield method, '><', '{{{a}}}', mydict(a='><')
 
 s = Stache()
 s.add_template('a', '1')
@@ -183,24 +192,24 @@ s.add_template('n', 'a{{?b}}b{{/}}{{?b}}b{{/}}')
 s.add_template('o', 'a{{?b}}b{{#b}}{{.}}{{/}}d{{/}}')
 
 def test_partials(method=bare_partial):
-    yield method, s, '1', 'a', dict()
-    yield method, s, '1', 'b', dict()
-    yield method, s, '11', 'c', dict()
-    yield method, s, '', 'd', dict()
-    yield method, s, '555', 'd', dict(a={'b':555})
-    yield method, s, '123', 'g', dict(a={})
-    yield method, s, '555', 'e', dict(a={'b':555})
-    yield method, s, '555', 'f', dict(a={'b':555})
-    yield method, s, 'i=123', 'h', dict()
-    yield method, s, 'i=', 'i', dict()
-    yield method, s, 'showme', 'j', dict()
-    yield method, s, 'default', 'k', dict()
-    yield method, s, 'custom', 'k', dict(e="custom")
+    yield method, s, '1', 'a', mydict()
+    yield method, s, '1', 'b', mydict()
+    yield method, s, '11', 'c', mydict()
+    yield method, s, '', 'd', mydict()
+    yield method, s, '555', 'd', mydict(a={'b':555})
+    yield method, s, '123', 'g', mydict(a={})
+    yield method, s, '555', 'e', mydict(a={'b':555})
+    yield method, s, '555', 'f', mydict(a={'b':555})
+    yield method, s, 'i=123', 'h', mydict()
+    yield method, s, 'i=', 'i', mydict()
+    yield method, s, 'showme', 'j', mydict()
+    yield method, s, 'default', 'k', mydict()
+    yield method, s, 'custom', 'k', mydict(e="custom")
     if not skip_bool:
         yield method, s, '<3><3><3>', 'l', {'id':3,'a':True, 'b':True}
         yield method, s, '<3><3><3>', 'm', {'id':3,'a?':True, 'b?':True}
-    yield method, s, 'abb', 'n', dict(b=[1,2,3])
-    yield method, s, 'ab123d', 'o', dict(b=[1,2,3])
+    yield method, s, 'abb', 'n', mydict(b=[1,2,3])
+    yield method, s, 'ab123d', 'o', mydict(b=[1,2,3])
 
 def test_js(method=verify_js):
     return
